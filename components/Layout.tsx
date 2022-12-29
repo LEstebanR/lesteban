c2bf02d372d28710130d36a508aa316ed98dc0bd
Luis Esteban Ramirez <leramirezca@gmail.com>
2022-12-28 22:42:40 -0500

    add layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,27 @@
 import { FC, ReactNode } from 'react'
+import styled from 'styled-components'
 import Header from './Header'
 import Page from './Page'
 interface Props {
   children?: ReactNode
 }
 
+const Container = styled.div`
+  background-color: red;
+  background-color: ${props => props.theme.color.background};
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+`
+
 const Layout: FC<Props> = ({ children }) => {
   return (
-    <>
+    <Container>
       <Header />
       <Page>
         <main>{children}</main>
       </Page>
-    </>
+    </Container>
   )
 }
 
--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,7 +1,14 @@
 const theme = {
   color: {
-    background: 'black'
-  }
+    background: '#0a192f',
+    primary: '#e8ff00'
+  },
+  fontSize: {
+    large: '3rem',
+    medium: '2rem',
+    small: '1rem'
+  },
+  sm: '992px'
 }
 
 export default theme
