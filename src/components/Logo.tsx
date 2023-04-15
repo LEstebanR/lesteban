8c6f055d6f6621eb0a001a5064404f312fdc4a59
Luis Esteban Ramírez <lesteban@Luiss-Mac-mini.local>
2023-04-14 21:55:26 -0500

    add header

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 import React, { FC } from 'react'
 
 const Logo: FC = () => {
-  return <Image src="/assets/images/logo.png" width={120} height={100} />
+  return (
+    <Image src="/assets/images/logo.png" width={120} height={100} alt="logo" />
+  )
 }
 
 export default Logo
