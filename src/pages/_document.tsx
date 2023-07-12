7ac6d34d2088344ff5fd023f4810b2beb90701c3
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-07-11 23:29:47 -0500

    add meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="LEsteban personal website"
-          title="LEsteban"
-        />
-      </Head>
+      <Head title="Lesteban" />
       <body className="bg-background">
         <Main />
         <NextScript />
