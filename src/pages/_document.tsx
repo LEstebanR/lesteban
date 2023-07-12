ea8cce4884aa19d1bbcd3bde975829f09af31c87
Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>
2023-07-11 23:22:20 -0500

    resolve conflicts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,13 +5,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-          name="description"
-          content="LEsteban personal website"
-          title="LEsteban"
-        />
-      </Head>
+      <div>
+        <Head title="LEsteban" />
+      </div>
       <body className="bg-background">
         <Main />
         <NextScript />
