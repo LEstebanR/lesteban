4b49e75e525306e3efa61c7d19157d9a149ab59b
Luis Esteban <leramirezca@gmail.com>
2023-11-10 22:11:40 -0500

    change projects card (#100)
    Co-authored-by: Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>

--- src/components/cards/projects-card.tsx
+++ src/components/cards/projects-card.tsx
@@ -12,38 +12,34 @@ interface Props {
 
 const ProjectsCard: FC<Props> = ({ project }) => {
   return (
-    <div className="flex  flex-col gap-4 rounded-lg  border border-l-8 border-black border-s-secondary  bg-light-card p-2  shadow-md dark:border-gray-200 dark:border-s-secondary dark:bg-dark-card">
-      <div className="flex flex-col gap-4 md:flex-row">
-        <div className="flex flex-col items-center gap-4 md:w-1/3">
-          <H4>{project.name}</H4>
-          <Image
-            src={project.image}
-            height={200}
-            width={200}
-            alt="project-logo"
-          />
-          <div className="flex w-full justify-center gap-4 text-3xl text-primary">
-            <Link href={project.repo} target="_blank">
-              <FiGithub />
-            </Link>
-            <Link href={project.url} target="_blank">
-              <FiLink />
-            </Link>
+    <div className="flex  flex-col justify-around gap-4 rounded-lg  border border-l-8 border-black border-s-secondary  bg-light-card p-2  shadow-md dark:border-gray-200 dark:border-s-secondary dark:bg-dark-card">
+      <div className="flex w-full flex-col items-center justify-around gap-1 ">
+        <H4>{project.name}</H4>
+
+        <Image
+          src={project.image}
+          height={1000}
+          width={1000}
+          alt="project-logo"
+          className="m-1 w-full rounded-lg"
+        />
+        <div className="flex w-full flex-col items-start">
+          <H4>Stack:</H4>
+          <div className="flex w-full flex-wrap justify-center gap-2">
+            {project.stack.map((tech: StackItem) => (
+              <StackItemLogo key={tech.name} tech={tech} />
+            ))}
           </div>
         </div>
-        <div className="flex flex-col md:w-2/3">
-          <CardBody>{project.description}</CardBody>
-        </div>
       </div>
-      <div className="md:flex md:items-center">
-        <span className="w-1/3">
-          <H4>Stack:</H4>
-        </span>
-        <div className="flex flex-wrap justify-between md:w-2/3">
-          {project.stack.map((tech: StackItem) => (
-            <StackItemLogo key={tech.name} tech={tech} />
-          ))}
-        </div>
+      <CardBody>{project.description}</CardBody>
+      <div className="flex w-full justify-end gap-4 text-3xl text-primary">
+        <Link href={project.repo} target="_blank">
+          <FiGithub />
+        </Link>
+        <Link href={project.url} target="_blank">
+          <FiLink />
+        </Link>
       </div>
     </div>
   )
