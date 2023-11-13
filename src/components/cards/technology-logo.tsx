4e4997ce07d21b804c33445a3eceb04eee8f2a17
Luis Esteban <leramirezca@gmail.com>
2023-11-13 17:05:21 -0500

    fix alerts (#106)
    * fix alerts
    
    * fix typo
    
    ---------
    
    Co-authored-by: Luis Esteban Ramirez <lesteban@Luiss-Mac-mini.local>

--- src/components/cards/technology-logo.tsx
+++ src/components/cards/technology-logo.tsx
@@ -15,8 +15,14 @@ interface Props {
 const TechnologyLogo: React.FC<Props> = ({ tech }) => {
   return (
     <div className="flex h-48 flex-col items-center  rounded-lg border border-black bg-light-card shadow-lg dark:border-gray-200 dark:bg-dark-card ">
-      <div className="flex h-40 w-full items-center justify-center p-2">
-        <Image src={tech.logo} width={100} height={100} alt="logo-tech" />
+      <div className="flex h-40  items-center justify-center p-2">
+        <Image
+          src={tech.logo}
+          alt="company logo"
+          height={100}
+          width={100}
+          className=" border-4 border-transparent"
+        />
       </div>
       <CardDetail>{tech.layer}</CardDetail>
     </div>
