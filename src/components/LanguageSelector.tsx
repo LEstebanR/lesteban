0975016119edf243d6d5a26fb3564277ae305f24
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-05-02 07:18:37 -0500

    update language selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -10,7 +10,7 @@ const LanguageSelector = () => {
   return (
     <button
       onClick={toggleLanguage}
-      className="flex h-8 w-8 cursor-pointer items-center justify-center rounded border border-gray-400 px-2 text-xs transition-colors hover:bg-gray-100"
+      className="flex h-6 w-6 cursor-pointer items-center justify-center rounded border border-gray-400 px-2 text-xs transition-colors hover:bg-gray-100"
     >
       {i18n.language.startsWith('en') ? t('ES') : t('EN')}
     </button>
