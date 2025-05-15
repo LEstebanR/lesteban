2dc7b116dbc85092719f7c8667e09ce9243694cc
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-05-14 22:17:21 -0500

    update header and logo

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,8 @@
 import { HEADER_LINKS } from '@data/data'
 import { Link } from './link'
-import { Subtitle } from '@components/ui/typography'
+import { Subtitle, H1 } from '@components/ui/typography'
 import LanguageSelector from '@components/LanguageSelector'
 import { useTranslation } from 'react-i18next'
-import { Logo } from '@components/ui/logo'
 
 type HeaderLink = {
   label: string
@@ -16,7 +15,7 @@ const Header = () => {
     <header className="fixed top-0 right-0 left-0 z-10 flex h-18 items-center justify-center border-b-2 border-gray-100 bg-white backdrop-blur-sm">
       <div className="flex w-full items-center justify-between px-4 lg:w-3/6 lg:px-0 2xl:w-2/6">
         <div className="w-1/2">
-          <Logo />
+          <H1 color="primary">Luis Esteban</H1>
           <Subtitle>{t('software-developer')}</Subtitle>
         </div>
         <div className="flex w-1/2 flex-wrap items-center justify-end gap-2 px-1">
