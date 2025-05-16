5cd5005155206d21b94774cc5ee8ad4737ecda9b
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-05-15 22:03:00 -0500

    fix deply problems

--- app/components/contact-card.tsx
+++ app/components/contact-card.tsx
@@ -1,16 +1,15 @@
 import { Body } from "@/app/components/typography";
+import { LucideIcon } from "lucide-react";
 
 export type ContactLink = {
   label: string;
-  href?: string;
+  href: string;
   user: string;
-  icon?: React.ReactNode;
-  iconColor?: string;
+  icon: LucideIcon;
+  iconColor: string;
 };
 
 export function ContactCard({ link }: { link: ContactLink }) {
-  const Icon = link.icon;
-
   return (
     <a href={link.href} target="_blank" rel="noopener noreferrer">
       <div
@@ -18,7 +17,7 @@ export function ContactCard({ link }: { link: ContactLink }) {
           link.href ? "cursor-pointer" : "cursor-default"
         } `}
       >
-        <Icon className={`${link.iconColor} h-6 w-6`} />
+        <link.icon className={`${link.iconColor} h-6 w-6`} />
         <div className="flex flex-col gap-1">
           <Body color="text-muted">{link.label}</Body>
           <Body>{link.user}</Body>
--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -7,8 +7,8 @@ export function Contact() {
     <div className="flex flex-col gap-4">
       <H1 color="primary">Contact</H1>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        {CONTACT_LINKS.map((link: ContactLink, index: number) => (
-          <ContactCard key={index} link={link} />
+        {CONTACT_LINKS.map((link, index) => (
+          <ContactCard key={index} link={link as ContactLink} />
         ))}
       </div>
     </div>
