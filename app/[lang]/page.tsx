ab0dd0d052aa188f6177079824cde8c46154f4f5
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-06-13 18:46:45 -0500

    fix accesibility

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -5,11 +5,14 @@ import { Contact } from '@/components/contact'
 import { Experience } from '@/components/experience'
 import { Projects } from '@/components/projects'
 import { Skills } from '@/components/skills'
+import { usePathname } from 'next/navigation'
 
 export default function Home() {
+  const pathname = usePathname()
+  const lang = pathname.split('/')[1] as 'en' | 'es'
   return (
     <div className="flex flex-col gap-12">
-      <AboutMe />
+      <AboutMe lang={lang} />
       <Experience />
       <Projects />
       <Skills />
