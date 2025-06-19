6dfd8d3c6b237e8fc016bf03929234a7f9da13e8
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-06-18 21:34:02 -0500

    fix og image

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -54,7 +54,7 @@ export const metadata: Metadata = {
         url: '/og-image.jpg',
         width: 1200,
         height: 630,
-        alt: 'Luis Esteban Ramirez - Software Developer',
+        alt: 'Luis Esteban Ramirez - Software Developer Portfolio',
       },
     ],
   },
