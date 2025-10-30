dcdd428784530aa36129df10b918ef305a5c9231
Luis Esteban Ramírez <leramirezca@gmail.com>
2025-10-29 22:50:23 -0500

    update icons

--- components/hero.tsx
+++ components/hero.tsx
@@ -39,7 +39,7 @@ export function Hero({ lang }: HeroProps) {
       <p className="text-muted-foreground mb-6 text-xl text-pretty sm:text-2xl md:mb-0">
         {dictionary['software-developer']}
       </p>
-      <p className="text-foreground/80 mx-auto mb-6 max-w-2xl text-justify text-lg leading-relaxed text-pretty md:mb-0">
+      <p className="text-foreground/80 mx-auto mb-6 max-w-2xl text-lg text-pretty md:mb-0 md:text-xl">
         {dictionary['about-me-description-1']}{' '}
         <span className="text-primary font-bold">
           {dictionary['global-impact']}
